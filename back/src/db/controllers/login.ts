b263fc240e95939fab52b8c749acf766b8ffec3a
matthieuGravy <matthieugravy@gmail.com>
2024-01-22 18:16:09 +0100

    login

--- back/src/db/controllers/login.ts
+++ back/src/db/controllers/login.ts
@@ -1,7 +1,5 @@
-/*
-
-import { Request, Response } from "express";
-import UserService from "../services/userService";
+import { IRegister } from "../models/register";
+import UserService from "../services/UserService"; // Assurez-vous que ce chemin est correct
 
 class LoginController {
   private userService: UserService;
@@ -10,23 +8,33 @@ class LoginController {
     this.userService = userService;
   }
 
-  async login(req: Request, res: Response) {
-    const { username, password } = req.body;
-
+  async login(
+    email: string,
+    password: string
+  ): Promise<{ user: IRegister; jwt: string } | null> {
     try {
-      const user = await this.userService.authenticate(username, password);
-      if (user) {
-        req.session.user = user;
-        res.status(200).send("Logged in successfully");
+      const register = await this.userService.findUserByEmail(email); // Utilisez userService ici
+
+      if (!register) {
+        console.log("User not found with email:", email);
+        return null; // L'utilisateur n'existe pas
+      }
+
+      const passwordMatch = await register.checkPassword(password);
+
+      if (passwordMatch) {
+        console.log("User logged in successfully:", email);
+        const jwt = await register.generateJWT(); // Générer un JWT pour l'utilisateur
+        return { user: register, jwt }; // Retourner l'utilisateur et le JWT
       } else {
-        res.status(401).send("Invalid username or password");
+        console.log("Invalid password for user:", email);
+        return null;
       }
     } catch (err) {
-      console.error(err);
-      res.status(500).send("Internal server error");
+      console.error("Error during login:", err);
+      throw err;
     }
   }
 }
-*/
 
-//export default LoginController;
+export default LoginController;
